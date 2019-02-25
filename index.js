20effb8fddfb1e28f3d798ef1f5514e7590651f4
stone <enrui.shi@stonybrook.edu>
2019-02-25 15:07:58 -0500

    fix path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = 3000
 
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'html/index.html'));
+    res.sendFile(path.join(__dirname+'/html/index.html'));
 })
 
 app.listen(port,'0.0.0.0', () => {
