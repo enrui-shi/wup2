ccb69520c94e39879ae13a10452bcf94161e7221
stone <enrui.shi@stonybrook.edu>
2019-02-25 15:39:29 -0500

    test1

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/html/index.html'));
+    res.send('GET route on things.');
+    //res.sendFile(path.join(__dirname+'/html/index.html'));
 })
 
 //export this router to use in our index.js
