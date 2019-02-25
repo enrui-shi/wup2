f51ce83463b3e6c6e5d45d20a1684e169b5c1bb2
stone <enrui.shi@stonybrook.edu>
2019-02-25 15:02:01 -0500

    add index.html

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
 
 app.listen(port,'0.0.0.0', () => {
     return console.log(`Example app listening on port ${port}!`);
