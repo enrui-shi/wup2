8ad72d551a480619497774fd529be08fe9c9abdd
stone <enrui.shi@stonybrook.edu>
2019-03-01 14:03:34 -0500

    convert form data to json 1.93

--- script/app.js
+++ script/app.js
@@ -2,12 +2,13 @@
 $(document).ready(function(){
     var login_form = $('#login');
     login_form.submit( function(e) {
+        var adduser_data = { name: $('#name').value };
         e.preventDefault();
         $.ajax({
             type: login_form.attr('method'),
             url: login_form.attr('action'),
             contentType:"application/json; charset=utf-8",
-            data: JSON.stringify(login_form.serializeArray()),
+            data: JSON.stringify(adduser_data),
             dataType:"json",
             success: function (data){
                 console.log(data);
