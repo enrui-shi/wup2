ede23524533a4ac22c62381927a12fcff6a17e82
stone <enrui.shi@stonybrook.edu>
2019-03-01 13:45:57 -0500

    convert form data to json 1.7

--- script/app.js
+++ script/app.js
@@ -1,21 +1,3 @@
-(function ($) {
-    $.fn.serializeFormJSON = function () {
-
-        var o = {};
-        var a = this.serializeArray();
-        $.each(a, function () {
-            if (o[this.name]) {
-                if (!o[this.name].push) {
-                    o[this.name] = [o[this.name]];
-                }
-                o[this.name].push(this.value || '');
-            } else {
-                o[this.name] = this.value || '';
-            }
-        });
-        return o;
-    };
-})(jQuery);
 
 $(document).ready(function(){
     var login_form = $('#login');
@@ -24,8 +6,7 @@ $(document).ready(function(){
         $.ajax({
             type: login_form.attr('method'),
             url: login_form.attr('action'),
-            data: login_form.serializeFormJSON(),
-            contentType:"application/json; charset=utf-8",
+            data: {name:"a",email:"b"},
             dataType:"json",
             success: function (data){
                 console.log(data);
