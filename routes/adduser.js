789c9789c97fa7b16d0fba2d98501e4b63d27f08
stone <enrui.shi@stonybrook.edu>
2019-03-01 17:07:00 -0500

    add redirect after create user

--- routes/adduser.js
+++ routes/adduser.js
@@ -8,19 +8,26 @@ var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-router.post('/',jsonParser,function(req,res){
+router.post('/',jsonParser,function(req,res,next){
     data = req.body;
     data['valide'] = "false";
     var db = req.app.locals.db;
     //add user to database
     db.collection("user").insertOne(data, function(err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
+        if (err) {
+            console.log(err);
+            res.json({ status:'ERROR'});
+        }else{
+            console.log("1 document inserted");
+        }
       });
     console.log(data);
-    res.json({ status:'OK'})
-    //res.send('adduser');
+    res.json({ status:'OK'});
+    next();
 })
+router.use(function (req, res) {
+    res.redirt('/')
+  });
 
 //export this router to use in our index.js
 module.exports = router;
