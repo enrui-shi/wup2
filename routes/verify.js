69ba1c9ab234f5f2464533dcd6daaa097bb9b931
stone <enrui.shi@stonybrook.edu>
2019-03-02 18:47:52 -0500

    test db query1.9999999

--- routes/verify.js
+++ routes/verify.js
@@ -10,10 +10,11 @@ router.post('/',jsonParser,function(req,res){
     console.log(data.email);
     console.log(data['key']);
     var db = req.app.locals.db;
-    db.collection('user').find({ 'email': data['email'] 
+    var result = db.collection('user').find({ 'email': data['email'] 
     }).toArray(function(err, result){
-          console.log(result);
-   });
+        //console.log(result);
+    });
+   console.log(result);
     res.json({ status:'OK'});
 });
 
