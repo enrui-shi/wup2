ddc84047e5e13c70c4bd7b5361ef3ccfd461c516
stone <enrui.shi@stonybrook.edu>
2019-03-02 19:17:17 -0500

    test update db

--- routes/verify.js
+++ routes/verify.js
@@ -8,11 +8,13 @@ router.post('/',jsonParser,function(req,res){
     data = req.body;
     json = {status:'OK'};
     console.log(data);
-    console.log(data.email);
-    console.log(data['key']);
+    //console.log(data.email);
+    //console.log(data['key']);
     var db = req.app.locals.db;
     db.collection('user').find({ 'email': data['email'] 
     }).toArray(function(err, result){
+        console.log(resutl.key);
+        console.log(data,key);
         if(result.key==data.key||result.key=='abracadabra'){
             console.log("verifed");
             db.collection('user').update({'email': data['email']},{ $set:
