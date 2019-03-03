2263c332fc94ed2a381969a7be25521f579e6a23
stone <enrui.shi@stonybrook.edu>
2019-03-02 19:42:23 -0500

    add mail function

--- routes/adduser.js
+++ routes/adduser.js
@@ -24,39 +24,7 @@ router.post('/',jsonParser,function(req,res){
             console.log(err);
         }else{
             console.log("1 document inserted");
-            //*************
-            var transporter = nodemailer.createTransport({
-                //host: 'email.cloud.compas.cs.stonybrook.edu',
-                host:'smtp.gmail.com',
-                port:465,
-                secure:true,
-                auth: {
-                    user: 'cse356test@gmail.com',
-                    pass: 'Cse356lalala'
-                }
-                // tls:{
-                //     //rejectUnauthorized:false
-                // }
-            });
-            var mailOpton = {
-                //from:'cse356@email.cloud.compas.cs.stonybrook.edu',
-                from: 'cse356test@gmail.com',
-                to: data.email,
-                subject: "test email",
-                text:"123"
-            };
-        
-            transporter.sendMail(mailOpton, function(error, info){
-                if (error) {
-                  console.log("error is:");
-                  console.log(error);
-                } 
-                  else{console.log('Email sent: ')}
-                });
-            
-            //********** 
-
-
+            sendMail(data);
         }
       });
     console.log(data);
@@ -68,7 +36,7 @@ module.exports = router;
 
 
 
-function sssss(){
+function sendMail(data){
     var transporter = nodemailer.createTransport({
         //host: 'email.cloud.compas.cs.stonybrook.edu',
         host:'smtp.gmail.com',
@@ -85,9 +53,9 @@ function sssss(){
     var mailOpton = {
         //from:'cse356@email.cloud.compas.cs.stonybrook.edu',
         from: 'cse356test@gmail.com',
-        to: email,
-        subject: "test email",
-        text:"123"
+        to: data.mail,
+        subject: "verify code",
+        text: data.key
     };
 
     transporter.sendMail(mailOpton, function(error, info){
