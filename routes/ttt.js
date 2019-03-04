5cf98ec58d2cb14913c936657ec229685178f498
stone <enrui.shi@stonybrook.edu>
2019-03-03 20:15:08 -0500

    add play.js

--- routes/ttt.js
+++ routes/ttt.js
@@ -13,15 +13,26 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 router.get('/',function(req,res){
     //res.send('GET route on things.');
     //console.log(req.session);
-    if(req.session.status == 'online'){
+    if(req.session.status == 'online' && req.session.current_user){
         res.sendFile(path.join(__dirname+'/..'+'/html/ttt.html'));
     }else{
+        req.session = null;
         res.redirect('/');
     }
 })
 
-router.post('/',jsonParser,function(req,res){
+router.post('/play',jsonParser,function(req,res){
     data = req.body;
+    console.log(data);
+    move = data.move;
+    db.collection('user').find({ 'username': req.session.current_user 
+    }).toArray(function(err, result){
+        result = result[0];
+        gird = result.current_grid;
+        //
+        json = play(move,gird);
+        res.json(json);
+    });
 });
 
 //export this router to use in our index.js
